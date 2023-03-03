fe20f8a0375a528926639b6b733bb91af4c1c160
DmitriyTruten <dmitriy2503a@gmail.com>
2023-03-03 13:26:04 +0200

    fetch weather data with current user location

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,34 @@
-import React from "react";
-import "./App.css"
+import React, { useState, useEffect } from "react";
+import "./App.css";
+export default function App() {
+  const [latitude, setLatitude] = useState([]);
+  const [longitude, setLongitude] = useState([]);
+  const [data, setData] = useState([]);
 
-function App() {
-  return <main className="app"></main>;
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      });
+
+      await fetch(
+        `${
+          import.meta.env.API_URL
+        }/weather?lat=${latitude}&lon=${longitude}&appid=${
+          import.meta.env.API_KEY
+        }`
+      )
+        .then((resolve) => resolve.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    fetchData();
+  }, [latitude, longitude]);
+
+  return <div className="App">
+    
+  </div>;
 }
-
-export default App;
