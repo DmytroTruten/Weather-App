5b7759edd83acc5dfd5ee466754ccf4b405de0b6
DmitriyTruten <dmitriy2503a@gmail.com>
2023-03-04 20:29:05 +0200

    adjust type of api request

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 export default function App() {
-  const [latitude, setLatitude] = useState([]);
-  const [longitude, setLongitude] = useState([]);
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetchWeatherData(latitude, longitude);
+      const response = await fetchWeatherData();
       setData(response);
       console.log(response);
     };
     fetchData();
-  }, [latitude, longitude]);
+  }, []);
 
-  const fetchWeatherData = async (lat, lon) => {
+  const fetchWeatherData = async () => {
     const response = await fetch(
-      `${import.meta.env.API_URL}/weather?lat=${lat}&lon=${lon}&appid=${
+      `${
+        import.meta.env.API_URL
+      }/weather?q=Kyiv&appid=${
         import.meta.env.API_KEY
       }&units=metric`
     );
@@ -24,18 +24,9 @@ export default function App() {
     return result;
   };
 
-  const handleLocationUpdate = (position) => {
-    setLatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude);
-  };
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(handleLocationUpdate);
-  }, []);
-
   return (
     <div className="App">
-      <p>{data.name}</p>
+      <p></p>
     </div>
   );
 }
