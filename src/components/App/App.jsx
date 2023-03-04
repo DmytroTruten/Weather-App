0eeab0f1ad8d426e2e82f30eb06109855c6f20b9
DmitriyTruten <dmitriy2503a@gmail.com>
2023-03-04 21:34:07 +0200

    add fetching data on city input

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 export default function App() {
   const [data, setData] = useState([]);
+  const [city, setCity] = useState("");
+  const didMount = useRef(true);
+
+  const fetchData = async () => {
+    const response = await fetchWeatherData();
+    setData(response);
+    console.log(response);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetchWeatherData();
-      setData(response);
-      console.log(response);
-    };
-    fetchData();
-  }, []);
+    if (didMount.current) {
+      didMount.current = false;
+    } else {
+      fetchData();
+    }
+  }, [city]);
 
   const fetchWeatherData = async () => {
     const response = await fetch(
-      `${
-        import.meta.env.API_URL
-      }/weather?q=Kyiv&appid=${
-        import.meta.env.API_KEY
+      `${import.meta.env.VITE_API_URL}/weather?q=${city}&appid=${
+        import.meta.env.VITE_API_KEY
       }&units=metric`
     );
     const result = await response.json();
     return result;
   };
 
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      const inputValue = event.target.value;
+      setCity(inputValue.charAt(0).toUpperCase() + inputValue.slice(1));
+    }
+  };
+
   return (
-    <div className="App">
-      <p></p>
+    <div className="app">
+      <input type="text" onKeyDown={handleKeyDown} />
+      <p>{city}</p>
     </div>
   );
 }
